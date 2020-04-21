1cb833bf9be7463170e109feceb1febca74c56c2
sally20921 <sally20921@github.com>
2020-04-22 08:58:01 +0900

    changes to Makefile

--- Makefile
+++ Makefile
@@ -1,36 +1,21 @@
-# (A) Optimized mode
- OPT ?= -O2 -DNDEBUG
-# (B) Debug mode
-# OPT ?= -g2
+OPT ?= -O2  -DNDEBUG
 
 TARGET=liblevelkv.so
 
 HOME=$(shell pwd)
 CC=gcc
-INCLUDES=-I$(HOME)/include
-LIBS=-L$(HOME)/libs -Wl,-rpath,$(HOME)/libs -lrt -lpthread -lleveldb -lkvssd -ltbb
+INCLUDES=-I$(HOME)/include 
+LIBS=-L$(HOME)/libs -Wl,-rpath,$(HOME)/libs -lrt -lpthread -lkvssd -ltbb 
 CXXFLAG=-fPIC -w -march=native -std=c++11 $(OPT)
 
-DB_SRCS=$(HOME)/src/kv_index_lsm.cc $(HOME)/src/kv_index_btree.cc $(HOME)/src/kv_index_base.cc $(HOME)/src/kv_index_inmem.cc $(HOME)/src/db_impl.cc $(HOME)/src/db_iter.cc
+DB_SRCS=$(HOME)/src/kv_index_atree.cc $(HOME)/src/kv_index_btree.cc $(HOME)/src/kv_index_base.cc $(HOME)/src/kv_index_inmem.cc $(HOME)/src/db_impl.cc $(HOME)/src/db_iter.cc
+
+KVATREE_SRCS=$(HOME)/src/atree/atree.cc $(HOME)/src/atree/write_batch.cc
 KVBTREE_SRCS=$(HOME)/src/kvbtree/bplustree.cc $(HOME)/src/kvbtree/cache.cc $(HOME)/src/kvbtree/hash.cc $(HOME)/src/kvbtree/write_batch.cc
 BASE_SRCS=$(HOME)/src/base/base.cc
 INMEM_SRCS=$(HOME)/src/inmem/inmem.cc
-KVSSD_SRCS=$(HOME)/src/kvssd/kvssd.cc
 UTIL_SRCS=$(HOME)/util/comparator.cc
-SRCS=$(DB_SRCS) $(KVBTREE_SRCS) $(BASE_SRCS) $(INMEM_SRCS) $(UTIL_SRCS)
+KVSSD_SRCS=$(HOME)/src/kvssd/kvssd.cc
 
-all: kvssd leveldb shared
+SRCS=$(DB_SRCS) $(KVATREE_SRCS) $(KVBTREE_SRCS) $(BASE_SRCS) $(INMEM_SRCS) $(UTIL_SRCS)
 
-kvssd:
-	$(CC) -shared -o $(HOME)/libs/libkvssd.so $(INCLUDES) $(KVSSD_SRCS) -L$(HOME)/libs -Wl,-rpath,$(HOME)/libs -lkvapi -lnuma $(CXXFLAG)
-
-leveldb:
-	make -C $(HOME)/src/leveldb/
-	cp $(HOME)/src/leveldb/out-shared/libleveldb.so* $(HOME)/libs
-
-shared:
-	$(CC) -shared -o $(HOME)/libs/$(TARGET) $(SRCS) $(INCLUDES) $(LIBS) $(CXXFLAG)
-
-clean:
-	make -C $(HOME)/src/leveldb/ clean
-	rm -rf $(HOME)/libs/$(TARGET) $(HOME)/libs/libkvssd.so $(HOME)/libs/libleveldb.so*
--- src/kv_index_atree.cc
+++ src/kv_index_atree.cc
@@ -1,8 +1,78 @@
 #include "kv_index.h"
 #include "levelkv/comparator.h"
-#include "kvbtree/atree.h"
-#include "kvbtree/write_batch.h"
+#include "atree/atree.h"
+#include "atree/write_batch.h"
 
 namespace levelkv {
-	
+class ComparatorATree: public kvatree::Comparator {
+private:
+	const levelkv::Comparator* cmp_;
+public:
+	ComparatorATree(const levelkv::Comparator* cmp) : cmp_(cmp) {};
+	~ComparatorATree() {};
+
+	int Compare(const kvatree::Slice& a, const kvatree::Slice& b) const {
+		Slice aa(a.data(), a.size());
+		Slice bb(b.data(), b.size());
+		return cmp_->Compare(aa, bb);
+	}
+};
+
+class IDXWriteBatchATree : public IDXWriteBatch {
+public:
+	kvatree::WriteBatch batch_;
+public:
+	IDXWriteBatchATree() {};
+	IDXWriteBatchATree() {};
+	void Put(const Slice& key) {
+		kvatree::Slice put_key(key.data(), key.size());
+		batch_.Put(put_key);
+	}
+	void  Delete(const Slice& key) {
+		kvatree::SLice del_key(key.data(), key.size());
+		batch_.Delete(del_key);
+	}
+	void Clear() {batch_.Clear();}
+	void *InternalBatch() {return &batch_;}
+
+};
+
+class IDXIteratorATree : public IDXIterator {
+private:
+	kvatree::KVBplusTree::Iterator *it_;
+public:
+	IDXIteratorATree(kvatree::KVBplusTree* db, const  ReadOptions& options) {
+	it_ = db->NewIterator();
+}
+~IDXIteratorATree() {delete  it_;}
+
+bool Valid() const {it_->Valid();}
+bool SeekToFirst() {it_->SeekToFirst();}
+bool SeekToLast() {}
+void Seek(const Slice& target) {
+	kvatree::Slice seek_key(target.data(), target.size());
+	it_->Seek(&seek_key);
+}
+void Next() {it_->Next();}
+void Prev() {}
+};
+
+};
+class KVIndexATree : public KVIndex {
+private:
+	kvatree::KVBplusTree *db_;
+	kvssd::KVSSD *kvd_;
+	kvbtree::Comparator *cmp_;
+
+public: 
+	KVIndexATree(const Options& options, kvssd::KVSSD* kvd);
+	~KVIndexATree();
+
+	//implmentations 
+	bool Put(const Slice& key);
+        bool Write(IDXWriteBatch* updates);
+	IDXIterator* NewIterator(const ReadOptions& options);
+};
+
+
 } //namespace levelkv
