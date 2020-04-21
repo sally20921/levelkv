fe521d9fc8a95e568a2fae61c8eaac04f9a814eb
Seri Lee <sally20921@snu.ac.kr>
2020-04-21 09:58:06 +0900

    Update unit_test.cc

--- src/bpt/unit_test.cc
+++ src/bpt/unit_test.cc
@@ -18,11 +18,20 @@ bplus_tree* tree = new bplus_tree();
 //assert(tree.meta.leaf_node_num == 1);
 //assert(tree.meta.height == 1);
 //PRINT("EmptyTree");
+assert(tree->meta.internal_node_num == 1);
+assert(tree->meta.leaf_node_num == 1);
+assert(tree->meta.height == 1);
+PRINT("EmptyTree");
 
 //assert(tree.insert("t2") == 0);
 //assert(tree.insert("t4") == 0);
 //assert(tree.insert("t1") == 0);
 //assert(tree.insert("t3") == 0);
+  
+assert(tree->insert("t2") == 0);
+assert(tree->insert("t4") == 0);
+assert(tree->insert("t1") == 0);
+assert(tree->insert("t3") == 0);
 
 //bpt::leaf_node_t leaf;
 //tree.map(&leaf, tree.search_leaf("t1"));
@@ -33,6 +42,16 @@ bplus_tree* tree = new bplus_tree();
 //assert(bpt::keycmp(leaf.children[3].key, "t4") == 0);
 
 //PRINT("Insert4Elements");
+  
+bpt::leaf_node_t leaf;
+tree.map(&leaf, tree->search_leaf("t1"));
+assert(leaf->n == 4);
+assert(bpt::keycmp(leaf->children[0].key, "t1") == 0);
+assert(bpt::keycmp(leaf->children[1].key, "t2") == 0);
+assert(bpt::keycmp(leaf->children[2].key, "t3") == 0);
+assert(bpt::keycmp(leaf->children[3].key, "t4") == 0);
+
+PRINT("Insert4Elements");
 
 return 0;
 }
